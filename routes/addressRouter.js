0e9c7dc85cb94f00723c707c9bc9f3d0b34d4cdf
FaruqKhan11148 <faruqkhanshired111@gmail.com>
2026-02-08 12:44:14 +0530

    updations

--- routes/addressRouter.js
+++ routes/addressRouter.js
@@ -7,31 +7,25 @@ router.use(protect);
 
 // pages
 router.get('/', async (req, res) => {
-  console.log('GET /addresses called', req.user._id);
   await addressController.checkoutAddressPage(req, res);
 });
 
 router.get('/new', (req, res) => {
-  console.log('GET /addresses/new called');
   addressController.addAddressPage(req, res);
 });
 
 router.get('/edit/:id', async (req, res) => {
-  console.log('GET /addresses/edit', req.params.id);
   await addressController.editAddressPage(req, res);
 });
 
 // actions
 router.post('/', async (req, res) => {
-  console.log('POST /addresses create', req.body);
   await addressController.createAddress(req, res);
 });
 router.post('/update/:id', async (req, res) => {
-  console.log('POST /addresses/update', req.params.id, req.body);
   await addressController.updateAddress(req, res);
 });
 router.post('/delete/:id', async (req, res) => {
-  console.log('POST /addresses/delete', req.params.id);
   await addressController.deleteAddress(req, res);
 });
 
